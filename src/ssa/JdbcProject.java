c17a46c5111ec39d01185c1e8489009b001220ea
Michael Clair <michaeldclair@gmail.com>
2016-09-14 16:12:45 -0400

    First commit of JdbcProject

---
+++ src/ssa/JdbcProject.java
@@ -0,0 +1,239 @@
+package ssa;
+
+import java.io.FileInputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class JdbcProject {
+
+//		static String url;
+//		public static final String userName="";
+//		public static final String pass="";
+		static Connection myConn=null;
+		static Statement stmt=null;
+		static ResultSet rs=null;
+		static int prereq;
+		static String reqstring;
+		ArrayList<int> columnName = new ArrayList<int>();
+		
+		public static void main(String[] args) throws SQLException {
+			
+			
+			try {
+				Properties props = new Properties();
+				props.load(new FileInputStream("jdbcProject.properties"));
+		
+				String theUser = props.getProperty("user");
+				String thePassword = props.getProperty("password");
+				String theDbUrl = props.getProperty("dburl");
+				
+	//			fetchData(theDbUrl, theUser, thePassword);
+	//			insertData(theDbUrl, theUser, thePassword);
+	//			fetchData(theDbUrl, theUser, thePassword);
+	//			fetchGeorge(theDbUrl, theUser, thePassword);
+	//			update(theDbUrl, theUser, thePassword);
+	//			fetchGeorge(theDbUrl, theUser, thePassword);
+	//			deleteRecord(theDbUrl, theUser, thePassword);
+	//			fetchData(theDbUrl, theUser, thePassword);
+	//			backup2(theDbUrl, theUser, thePassword);
+				backup3(theDbUrl, theUser, thePassword);
+
+			
+			}
+			catch(Exception exc){
+					exc.printStackTrace();
+			}
+			finally {
+				if(myConn!=null)
+					myConn.close();
+				if(stmt!=null)
+					stmt.close();
+			}
+		}
+		public static void close(Connection con, Statement stmt, ResultSet rs) throws SQLException {
+			if(con!=null)
+				con.close();
+			if(stmt!=null)
+				stmt.close();
+			if(rs!=null)
+				rs.close();}
+		
+		public static void fetchData(String url, String user, String pass) throws SQLException {
+			try {
+				myConn = DriverManager.getConnection(url, user, pass);
+				stmt = myConn.createStatement();
+				rs = stmt.executeQuery("select * from student;");
+				while(rs.next()) {
+					System.out.println("First Name " + rs.getString("first_name") + " Last Name: " + rs.getString("last_name") + " id " + rs.getInt("id") + " GPA: " + rs.getDouble("gpa"));
+				}
+			}catch(Exception exc){
+				exc.printStackTrace();
+			}finally {
+				close(myConn, stmt, rs);}
+		}
+		
+		public static void update(String url, String user, String pass) throws SQLException{
+			try {
+				myConn = DriverManager.getConnection(url, user, pass);
+				stmt = myConn.createStatement();
+				stmt.executeUpdate("update student set gpa = 3.5, sat = 1450, major_id = 1 where id = 13");
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}finally {
+				if(myConn!=null)
+					myConn.close();
+				if(stmt!=null)
+					stmt.close();}
+		}
+		
+		public static void insertData(String url, String user, String pass) throws SQLException{
+			try {
+				myConn = DriverManager.getConnection(url, user, pass);
+				stmt = myConn.createStatement();
+				stmt.executeUpdate("insert student values (13, 'George', 'Washington',4.0,1400,NULL)");
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}finally {
+				if(myConn!=null)
+					myConn.close();
+				if(stmt!=null)
+					stmt.close();}
+		}
+		
+		public static void deleteRecord(String url, String user, String pass) throws SQLException{
+			try {
+				myConn = DriverManager.getConnection(url, user, pass);
+				stmt = myConn.createStatement();
+				stmt.executeUpdate("delete from student where last_name = 'Washington' and sat = 1450;");
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}finally {
+				if(myConn!=null)
+					myConn.close();
+				if(stmt!=null)
+					stmt.close();}
+		}
+		
+		public static void fetchGeorge(String url, String user, String pass) throws SQLException {
+			try {
+				myConn = DriverManager.getConnection(url, user, pass);
+				stmt = myConn.createStatement();
+				rs = stmt.executeQuery("select * from student where first_name = 'George' and last_name = 'Washington';");
+				System.out.println("First Name  Last Name   GPA   SAT  Major-Code ");
+				while(rs.next())
+				System.out.println(rs.getString("first_name") + "      " + rs.getString("last_name") + "  " + rs.getDouble("gpa") + "  " + rs.getInt("sat") + "       " + rs.getInt("major_id"));
+			}catch(Exception exc){
+					exc.printStackTrace();
+			}finally {
+					close(myConn, stmt, rs);}
+		}
+		
+		
+		public static void backup(String url, String user, String pass) throws SQLException {
+			try {
+				myConn = DriverManager.getConnection(url, user, pass);
+				stmt = myConn.createStatement();
+				rs = stmt.executeQuery("select * from student;");
+				while(rs.next())
+				
+				
+				System.out.println("Insert student values (" + rs.getInt("id") + ", '" + rs.getString("first_name") + "', '" + rs.getString("last_name") + "', " + rs.getDouble("gpa") + ", " + rs.getInt("sat") + ", " + rs.getInt("major_id") + ");");
+			}catch(Exception exc){
+					exc.printStackTrace();
+			}finally {
+					close(myConn, stmt, rs);}
+		}
+		
+		
+		
+		public static void backup2(String url, String user, String pass) throws SQLException {
+			try {
+				myConn = DriverManager.getConnection(url, user, pass);
+				stmt = myConn.createStatement();
+				
+				//Backup of Major
+				rs = stmt.executeQuery("select * from major;");
+				while(rs.next())
+				System.out.println("Insert major values (" + rs.getInt("id") + ", '" + rs.getString("description") + "', " + rs.getInt("minimum_sat") + ");");
+				
+				//Backup of Student
+				rs = stmt.executeQuery("select * from student;");
+				while(rs.next())
+					System.out.println("Insert student values (" + rs.getInt("id") + ", '" + rs.getString("first_name") + "', '" + rs.getString("last_name") + "', " + rs.getDouble("gpa") + ", " + rs.getInt("sat") + ", " + rs.getInt("major_id") + ");");
+				
+				//Backup of Instructor
+				rs = stmt.executeQuery("select * from instructor;");
+				while(rs.next())
+					System.out.println("Insert instructor values (" + rs.getInt("id") + ", '" + rs.getString("first_name") + "', '" + rs.getString("last_name") + "', " + rs.getInt("major_id") + ", " + rs.getInt("years_experience") + ", " + rs.getInt("tenure") + ");");
+				
+				//Backup of Class
+				rs = stmt.executeQuery("select * from class;");
+				while(rs.next())
+					System.out.println("Insert class values (" + rs.getInt("id") + ", '" + rs.getString("description") + "', " + rs.getInt("instructor_id") + ");");
+				
+				//Backup of Grade
+				rs = stmt.executeQuery("select * from grade;");
+				while(rs.next())
+					System.out.println("Insert grade values (" + rs.getInt("id") + ", '" + rs.getString("description") + "');");
+				
+				//Backup of Assignment
+				rs = stmt.executeQuery("select * from assignment;");
+				while(rs.next())
+					System.out.println("Insert assignment values (" + rs.getInt("id") + ", " + rs.getInt("student_id") + ", " + rs.getInt("assignment_nbr") + ", " + rs.getInt("grade_id") + ", " + rs.getInt("class_id") + ");");
+			
+				//Backup of Major_Class_Relationship
+				rs = stmt.executeQuery("select * from major_class_relationship;");
+				while(rs.next())
+					System.out.println("Insert major_class_relationship values (" + rs.getInt("id") + ", " + rs.getInt("major_id") + ", " + rs.getInt("class_id") + ");");
+				
+				//Backup of Student_Class_Relationship
+				rs = stmt.executeQuery("select * from student_class_relationship;");
+				while(rs.next())
+					System.out.println("Insert student_class_relationship values (" + rs.getInt("id") + ", " + rs.getInt("student_id") + ", " + rs.getInt("class_id") + ");");
+				
+				//Backup of Prerequisite
+				rs = stmt.executeQuery("select * from prerequisite;");
+
+				while(rs.next())
+//				prereq = rs.getInt("prereq");
+//				System.out.println(rs.getInt("prereq"));
+//					if (prereq < 1) {
+//						reqstring = "NULL";}
+//					else {
+//						reqstring = String.valueOf(prereq);
+//					}
+					System.out.println("Insert prerequisite values (" + rs.getInt("id") + ", " + rs.getInt("class_id") + ", " + rs.getInt("prereq") + ");");
+				
+				
+				
+				
+				
+			}catch(Exception exc){
+					exc.printStackTrace();
+			}finally {
+					close(myConn, stmt, rs);}
+		}
+		
+		
+		public static void backup3(String url, String user, String pass) throws SQLException {
+			try {
+				myConn = DriverManager.getConnection(url, user, pass);
+				stmt = myConn.createStatement();
+				rs = stmt.executeQuery("select * from student;");
+				ResultSetMetaData rsmd = rs.getMetaData();
+				int numberOfColumns = rsmd.getColumnCount();
+				for (int i = 0, i < numberOfColumns, i++) {
+					
+				}
+				String columnName = rsmd.getColumnName(1);
+				System.out.println(numberOfColumns);
+				System.out.println(columnName);
+			
+			}catch(Exception exc){
+					exc.printStackTrace();
+			}finally {
+					close(myConn, stmt, rs);}
+		}
+	}
+
